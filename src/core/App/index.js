2b649b4342396a21385f91c979e46fea65803f48
Vitalii Tomenko <vitalikxxx985@gmail.com>
2025-02-17 21:53:19 +0100

    Add App Component

---
+++ src/core/App/index.js
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "styled-components";
+
+export const App = () => {
+    return (
+        <ThemeProvider>
+
+        </ThemeProvider>
+    );
+};
+
+export default App;
