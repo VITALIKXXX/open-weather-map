a269d732dda90b761e7cf81b0f6b4403980e8620
Vitalii Tomenko <vitalikxxx985@gmail.com>
2025-02-18 18:06:04 +0100

    Import Global Style to App

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { Normalize } from "styled-normalize";
+import { GlobalStyle } from "./GlobalStyle";
 
 export const App = () => {
     return (
         <ThemeProvider>
             <Normalize />
+            <GlobalStyle />
 
         </ThemeProvider>
     );
