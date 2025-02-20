f13c4c5776e53f012ee56f1192c0780373527a3a
Vitalii Tomenko <vitalikxxx985@gmail.com>
2025-02-20 18:17:46 +0100

    New URL and catch error

--- src/features/weather/WeatherService.js
+++ src/features/weather/WeatherService.js
@@ -1,16 +1,22 @@
 import axios from "axios";
 
 const API_KEY = "4f1ea456aa3466ee9b4705d4305ed2de";
-const URL = "https://openweathermap.org/";
+const URL = "https://api.openweathermap.org/data/2.5/weather";
 
 export const getWeather = async (city) => {
-    const response = await axios.get(URL, {
-        params: {
-            q: city,
-            appid: API_KEY,
-            units: "metric",
-            lang: "pl"
-        }
-    });
-    return response.data;
+    try {
+        const response = await axios.get(URL, {
+            params: {
+                q: city,
+                appid: API_KEY,
+                units: "metric",
+                lang: "pl"
+            }
+        });
+
+        return response.data;
+    } catch (error) {
+        console.error("Błąd pobierania pogody:", error.response?.data || error.message);
+        return null;
+    }
 };
