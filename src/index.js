ef31ee70fb92484c1691a6f859842331a5c3e258
Vitalii Tomenko <vitalikxxx985@gmail.com>
2025-02-21 17:06:21 +0100

    Create Component App

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './core/App/index';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from "./core/App/store";
+import GlobalStyle from './core/App/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './core/App/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </Provider>
 );
 
 reportWebVitals();
