f350dae6b696f4e519483f6d8571210309805f44
Vitalii Tomenko <vitalikxxx985@gmail.com>
2025-02-22 08:02:22 +0100

    add theme mode

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,14 +1,19 @@
 import { ThemeProvider } from "styled-components";
 import { Normalize } from "styled-normalize";
+import { useSelector } from "react-redux";
 import { GlobalStyle } from "./GlobalStyle";
 import { Weather } from "../../features/weather";
-import theme from "./theme";
+import { darkTheme, lightTheme } from "./theme";
+import ThemeSwitch from "../../common";
 
 export const App = () => {
+    const darkMode = useSelector((state) => state.theme.darkMode);
+
     return (
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
             <Normalize />
             <GlobalStyle />
+            <ThemeSwitch />
             <Weather />
         </ThemeProvider>
     );
