a20d097bec4e9557ecbf4a1b6409bf0ef3911ba5
Vitalii Tomenko <vitalikxxx985@gmail.com>
2025-02-23 22:42:04 +0100

    Edyt store.js

--- src/core/App/store.js
+++ src/core/App/store.js
@@ -1,10 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
 import themeReducer from "../../common/themeSlice";
+import weatherReducer from "../../features/weather/Weather/weatherSlice";
+import rootSaga from "./saga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         theme: themeReducer,
+        weather: weatherReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
